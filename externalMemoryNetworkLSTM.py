e2a1ef51d208c87c8f1e30605e34db43e3e33a5e
Karishma Malkan <karishmamalkan@lawn-128-61-26-170.lawn.gatech.edu>
2016-07-12 14:07:14 -0400

    latest changes

--- externalMemoryNetworkLSTM.py
+++ externalMemoryNetworkLSTM.py
@@ -5,17 +5,18 @@ import math
 
 floatX=theano.config.floatX
 class ExternalMemoryNetwork:
-    def __init__(self, N, vocabSize,hidden_dim, numLabels, learningRate):
+    def __init__(self, vocabSize,embeddingSize, hidden_dim, numLabels):
 
         self.params = collections.OrderedDict()
         self.h = hidden_dim
-        self.wordEmbeddingSize = vocabSize
-        self.learningRate = learningRate
-        l2_regularisation = 0.0001 #todo check why this is giving nans for higher vals
+        self.wordEmbeddingSize = embeddingSize
+
+        l2_regularisation = 0.001 #todo check why this is giving nans for higher vals
         random_seed=42
         # random number generator
         self.rng = np.random.RandomState(random_seed)
 
+        learningRate = theano.tensor.fscalar('learningRate')
         arg1Embeddings = theano.tensor.imatrix('arg1Embeddings')
         erase = theano.shared(np.ones((1,self.h)), name='erase')
         targetClass = theano.tensor.iscalar('targetClass')
@@ -37,23 +38,23 @@ class ExternalMemoryNetwork:
             LSTMNumber=str(LSTMNumber)
 
             #input gate
-            Whi = self.createParameterMatrix('Whi_'+LSTMNumber, hhTuple, "Uniform")
-            Wxi= self.createParameterMatrix('Wxi_'+LSTMNumber, xhTuple,"Uniform")
+            Whi = self.createParameterMatrix('Whi_'+LSTMNumber, hhTuple, "Ortho")
+            Wxi= self.createParameterMatrix('Wxi_'+LSTMNumber, xhTuple,"Ortho")
             bi= self.createParameterMatrix('bi_'+LSTMNumber, hTuple)
 
             #forget gate
-            Whf = self.createParameterMatrix('Whf_'+LSTMNumber, hhTuple, "Uniform")
-            Wxf = self.createParameterMatrix('Wxf_'+LSTMNumber, xhTuple,"Uniform")
+            Whf = self.createParameterMatrix('Whf_'+LSTMNumber, hhTuple, "Ortho")
+            Wxf = self.createParameterMatrix('Wxf_'+LSTMNumber, xhTuple,"Ortho")
             bf = self.createParameterMatrix('bf_'+LSTMNumber, hTuple)
 
             #output gate
-            Who = self.createParameterMatrix('Who_'+LSTMNumber, hhTuple, "Uniform")
-            Wxo = self.createParameterMatrix('Wxo_'+LSTMNumber, xhTuple,"Uniform")
+            Who = self.createParameterMatrix('Who_'+LSTMNumber, hhTuple, "Ortho")
+            Wxo = self.createParameterMatrix('Wxo_'+LSTMNumber, xhTuple,"Ortho")
             bo = self.createParameterMatrix('bo_'+LSTMNumber, hTuple)
 
             #input gate gate
-            Whg = self.createParameterMatrix('Whg_'+LSTMNumber, hhTuple, "Uniform")
-            Wxg = self.createParameterMatrix('Wxg_'+LSTMNumber, xhTuple,"Uniform")
+            Whg = self.createParameterMatrix('Whg_'+LSTMNumber, hhTuple, "Ortho")
+            Wxg = self.createParameterMatrix('Wxg_'+LSTMNumber, xhTuple,"Ortho")
             bg = self.createParameterMatrix('bg_'+LSTMNumber, hTuple)
 
 
@@ -70,13 +71,14 @@ class ExternalMemoryNetwork:
         Whi_2, Wxi_2, bi_2, Whf_2, Wxf_2, bf_2, Who_2, Wxo_2, bo_2, Whg_2, Wxg_2, bg_2 = getLSTMParameters(2)
         WV2 = self.createParameterMatrix('WV2', (numLabels, self.h, vocabSize))
         bV = self.createParameterMatrix('bV', (numLabels, vocabSize))
+        Wh_lh = self.createParameterMatrix('Whl', (numLabels, self.h))
 
 
         def LSTM_arg1(x, prevH, prevCt, prevMB, Whi_1, Wxi_1, bi_1, Whf_1, Wxf_1, bf_1, Who_1, Wxo_1, bo_1, Whg_1, Wxg_1, bg_1, erase):
             #calculate next hidden state
-            i = theano.tensor.nnet.sigmoid(theano.tensor.dot( prevH,Whi_1) +bi_1+ theano.tensor.dot(x, Wxi_1))
-            f = theano.tensor.nnet.sigmoid(theano.tensor.dot( prevH,Whf_1) +bf_1+ theano.tensor.dot(x, Wxf_1))
-            o = theano.tensor.nnet.sigmoid(theano.tensor.dot( prevH,Who_1) +bo_1+ theano.tensor.dot(x, Wxo_1))
+            i = theano.tensor.nnet.hard_sigmoid(theano.tensor.dot( prevH,Whi_1) +bi_1+ theano.tensor.dot(x, Wxi_1))
+            f = theano.tensor.nnet.hard_sigmoid(theano.tensor.dot( prevH,Whf_1) +bf_1+ theano.tensor.dot(x, Wxf_1))
+            o = theano.tensor.nnet.hard_sigmoid(theano.tensor.dot( prevH,Who_1) +bo_1+ theano.tensor.dot(x, Wxo_1))
 
             g = theano.tensor.tanh( theano.tensor.dot(prevH, Whg_1) + bg_1 + theano.tensor.dot(x, Wxg_1) )
             ct = (f * prevCt) + (g * i)
@@ -90,7 +92,9 @@ class ExternalMemoryNetwork:
             normalizedWeightDistribution = normalizedWeightDistribution.reshape((1,normalizedWeightDistribution.shape[0]))
 
             #erase memory block
-            curMB = prevMB - theano.tensor.dot(theano.tensor.transpose(normalizedWeightDistribution), erase)
+            # curMB = prevMB - theano.tensor.dot(theano.tensor.transpose(normalizedWeightDistribution), erase)
+            curMB = prevMB - theano.tensor.dot(theano.tensor.transpose(normalizedWeightDistribution), prevMB)#todo try
+            # curMB = prevMB - theano.tensor.dot(theano.tensor.transpose(normalizedWeightDistribution), prevMB)#todo try
 
             #write to memory block {normalizedWeightDistribution:(1,20),
             curMB = curMB + theano.tensor.dot(theano.tensor.transpose(normalizedWeightDistribution), h.reshape((1,h.shape[0])))
@@ -110,10 +114,11 @@ class ExternalMemoryNetwork:
 
             g = theano.tensor.tanh( theano.tensor.batched_dot(prevH,Whg_2) + theano.tensor.dot(curWord, Wxg_2) + bg_2)
             ct = (f * prevCt) + (g * i)
-            h = theano.tensor.tanh( ct ) * o
+            h = theano.tensor.tanh( ct ) * o #l*h
 
-            MBFrac=0.1
-            hForPredict = (1-MBFrac) * h + MBFrac * pooledMemoryUnit
+            MBFrac=0.5 #todo change back
+            # MBFrac=0.5 #todo change back
+            hForPredict = theano.tensor.nnet.sigmoid((1-MBFrac) * h + MBFrac * pooledMemoryUnit) # l*h + h -> l*h
 
             probOverWords = theano.tensor.nnet.softmax(theano.tensor.batched_dot(hForPredict,WV)+bV) #softmax{(l*h) * (l*h*V) -> (l*V)}
             probNextWord =  theano.tensor.log(probOverWords[:,nextWordIndex]) # array([l])
@@ -131,13 +136,16 @@ class ExternalMemoryNetwork:
 
         def calcProbArg2AllLabels(MBState):
             pooledMemoryUnit = MBState[theano.tensor.argmax(theano.tensor.square(MBState).sum(axis=1))]
+
+            # pooledMemoryUnit = theano.tensor.nnet.sigmoid(Wh_lh * pooledMemoryUnit)#todo bring back
             #todo remove target class as initial scalar
             initialHiddenVector = theano.tensor.alloc(np.array(0, dtype=floatX), numLabels,self.h) #todo define centrally
 
             initHiddenProbability = theano.shared(np.zeros(numLabels,dtype=floatX), name="initHiddenProbability")
-            (_,_,arg2Probs),_ = theano.scan(LSTM_arg2,sequences = [arg2Embeddings[0:-1], arg2Indices[1:]],
+            # (_,_,arg2Probs),_ = theano.scan(LSTM_arg2,sequences = [arg2Embeddings[0:-1], arg2Indices[0:-1]],
+            (_,_,arg2Probs),_ = theano.scan(LSTM_arg2,sequences = [arg2Embeddings[0:-1], arg2Indices[1:]],#todo change back to get the word order
                                             outputs_info = [initialHiddenVector, initialHiddenVector, initHiddenProbability],
-                                            non_sequences=[Whi_2, Wxi_2, bi_2, Whf_2, Wxf_2, bf_2, Who_2, Wxo_2, bo_2, Whg_2, Wxg_2, bg_2, WV2, bV, pooledMemoryUnit])
+                                            non_sequences=[Whi_2, Wxi_2, bi_2, Whf_2, Wxf_2, bf_2, Who_2, Wxo_2, bo_2, Whg_2, Wxg_2, bg_2, WV2, bV, pooledMemoryUnit])#todo remove 2 from pooled unit
 
             probArg2Total=theano.tensor.nnet.softmax(arg2Probs.sum(axis=0))[0]
             return probArg2Total
@@ -147,10 +155,12 @@ class ExternalMemoryNetwork:
             finalLabelDist1, MBState = calcHArg1()
             #todo add so that memory is pooled based on which label is correct
             probArg2Total = calcProbArg2AllLabels(MBState)
-
+            frac1 = 0.85
             #todo bring back
             # logProbabilityOfTargetLabel=    theano.tensor.nnet.softmax(finalLabelDist1 + probArg2Total)[0]
-            logProbabilityOfTargetLabel =  theano.tensor.nnet.softmax(theano.tensor.log(finalLabelDist1) + theano.tensor.log( probArg2Total))[0]
+            # logProbabilityOfTargetLabel =  theano.tensor.nnet.softmax(theano.tensor.log(finalLabelDist1) + theano.tensor.log( probArg2Total))[0]
+            # logProbabilityOfTargetLabel =  theano.tensor.nnet.softmax(theano.printing.Print('p1')((1-frac1)*finalLabelDist1) * theano.printing.Print('p2')(frac1*probArg2Total))[0]#todo bring back
+            logProbabilityOfTargetLabel =  theano.tensor.nnet.softmax((1-frac1)* theano.tensor.log(finalLabelDist1) + frac1*theano.tensor.log(probArg2Total))[0]#todo bring back
             # logProbabilityOfTargetLabel=    theano.tensor.nnet.softmax(theano.tensor.log(finalLabelDist1) + theano.tensor.log(probArg2Total))[0]
 
 
@@ -173,22 +183,39 @@ class ExternalMemoryNetwork:
 
         def getUpdates(cost):
             gradients = theano.tensor.grad(cost,self.params.values())
-            updates = [(w, w - self.learningRate*g) for w,g in zip(self.params.values(),gradients)]
+            updates = [(w, w - learningRate*g) for w,g in zip(self.params.values(),gradients)]
             return updates
 
         cost,predictedClass = trainExampleCE()
         updates=getUpdates(cost)
 
         #calculate the update to weights based on the cost function
-        self.train = theano.function([arg1Embeddings, arg2Embeddings, arg2Indices,oneHotLabel, initMB], [predictedClass,cost], updates = updates, allow_input_downcast=True)
+        self.train = theano.function([arg1Embeddings, arg2Embeddings, arg2Indices,oneHotLabel, initMB, learningRate], [predictedClass,cost], updates = updates, allow_input_downcast=True)
 
         #calculate the cost at the iteration but dont make any updates to the weights
         self.train_no_update = theano.function([arg1Embeddings, arg2Embeddings, arg2Indices,oneHotLabel, initMB], [predictedClass,cost], allow_input_downcast=True)
 
     def createParameterMatrix(self,name,size,type="Normal"):
-        if type == "Uniform":
+        if type == "Ortho":
+            randomMatrix = np.random.uniform(low = - math.sqrt(6./sum(size)), high=math.sqrt(6./sum(size)), size=size)
+            # randomMatrix = self.rng.normal(loc=0.0, scale=0.1, size=size)
+            u, s, vt = np.linalg.svd(randomMatrix, full_matrices = False)
+            q = u if u.shape == size else vt
+            vals = q.reshape(size)
+        elif type == "Uniform":
             vals = np.asarray(np.random.uniform(low = - math.sqrt(6./sum(size)), high=math.sqrt(6./sum(size)), size=size), dtype=floatX)
         else:
-            vals = np.asarray(self.rng.normal(loc=0.0, scale=0.1, size=size), dtype=floatX)
+            vals = np.asarray(self.rng.normal(loc=0.0, scale=0.1, size=size), dtype=floatX)# try scale = 1 by root m
         self.params[name] = theano.shared(vals, name)
         return self.params[name]
+
+#diff
+'''
+Ideas:
+1. change to dclm model
+2. the way next word is predcted. Not just that word, but anything like that word
+1. switching to ortho inits. Not yet done though
+2. remove bias of model
+3. changed to MSE
+4. Should we try.. predict the cosine of the next word for each label?
+ '''
--- train.py
+++ train.py
@@ -1,91 +1,122 @@
+from __future__ import print_function
 import data
 import numpy as np
-import externalMemoryNetwork
-import externalMemoryNetworkLSTM2
+# import externalMemoryNetwork
+import externalMemoryNetworkLSTM
 import theano
 import pickle
 from collections import Counter
+from gensim.models import word2vec
+import math
 
-filename="../data/correct_data/trn-levelone.txt.10K"
-# filename="../data/correct_data/dev-levelone.txt.10K"
-test_filename="../data/correct_data/tst-levelone.txt.10K"
-
-
-# filename="../data/pdtb-for-naacl2016/dev-levelone.txt"
-# test_filename="../data/pdtb-for-naacl2016/tst-levelone.txt"
-# vocabSize=1000#300
-vocabSize=300
-# vocabSize=100
-totalIts=40
-MBSlots=20
-# hiddenDim=30#6
-hiddenDim=6
-# hiddenDim=3
-MBSize=(MBSlots,hiddenDim)
-numLabels=4
-learningRate=0.02
-# learningRate=0.05#85% accuracy
-floatX=theano.config.floatX
-
-
-vocabSize,vocab,trainingSetWordFormat,trainingSetIndicesFormat=data.createTrainingSetAndBuildVocab(filename,vocabSize)
-testSetWordFormat,testSetIndicesFormat=data.createTestSet(test_filename,vocab)
-with open('testTrain-'+str(vocabSize), 'w') as f:  # Python 3: open(..., 'wb')
-    pickle.dump([vocabSize,vocab,trainingSetWordFormat,trainingSetIndicesFormat,testSetWordFormat,testSetIndicesFormat], f)
-# with open('testTrain-'+str(vocabSize+3)) as f:  # Python 3: open(..., 'rb')
-#     vocabSize,vocab,trainingSetWordFormat,trainingSetIndicesFormat,testSetWordFormat,testSetIndicesFormat = pickle.load(f)
-
-
-
-wordEmbeddings=np.identity(vocabSize)
-random_seed = 42
-rng = np.random.RandomState(random_seed)
-initMB = np.asarray(rng.normal(loc=0.0, scale=0.1, size=MBSize), dtype=floatX)
-# emn=externalMemoryNetwork.ExternalMemoryNetwork(MBSize, vocabSize, hiddenDim, numLabels, learningRate)
-emn=externalMemoryNetworkLSTM2.ExternalMemoryNetwork(MBSize, vocabSize, hiddenDim, numLabels, learningRate)
-
-
-def trainOrTest(dataSetIndicesFormat,updateWeights = True ,printType = "Train"):
+def trainOrTest(dataSetIndicesFormat,dataSetEmbeddingsFormat,updateWeights = True ,printType = "Train", learningRate=None):
     mcc=Counter()
-    totalEgs=0
+    totalEgs=len(dataSetIndicesFormat)
     totalCost=0
     correct_before_update=0
-    for (arg1,arg2,target) in dataSetIndicesFormat:
+    for index,(arg1,arg2,target) in enumerate(dataSetIndicesFormat):
+        if printType=="Train":
+            print('Processed: ',index,' of ', totalEgs, end='\r')
+
+        arg1Embeddings,arg2Embeddings,_ = dataSetEmbeddingsFormat[index] #todo change to access index if necessary
+         # = dataSetEmbeddingsFormat[index][1]
         oneHotLabel=np.zeros((numLabels))
         oneHotLabel[target]=1
+
         if updateWeights==True:
-            _, _ = emn.train(wordEmbeddings[arg1],wordEmbeddings[arg2],arg2,target,initMB)
+            _,_ = emn.train(arg1Embeddings,arg2Embeddings,arg2,oneHotLabel,initMB, learningRate)
         else:
-            predicted,cost = emn.train_no_update(wordEmbeddings[arg1],wordEmbeddings[arg2],arg2,target,initMB)
+            predicted,cost = emn.train_no_update(np.array(arg1Embeddings),np.array(arg2Embeddings),arg2,oneHotLabel,initMB)
             mcc.update({target:1})
             if int(predicted) == int(target):
                 correct_before_update+=1
-
-            totalEgs+=1
             totalCost+=cost
+
 
     if not updateWeights:
         if printType=="Train":
-            print "%s Iter:%d \tCost:%f\tAccuracy:%f\tMCC: %f"\
-          %(printType, iter, totalCost, correct_before_update*1.0/totalEgs, mcc.most_common(1)[0][1]*1.0/totalEgs),
+            print('                                                                                        ', end='\r')
+            print("%s Iter:%d \tCost:%f\tAccuracy:%f\tMCC: %f"\
+          %(printType, iter, totalCost, correct_before_update*1.0/totalEgs, mcc.most_common(1)[0][1]*1.0/totalEgs),end='\t')
         else:
-            print "Test   Cost:%f\tAccuracy:%f\tMCC: %f"%(totalCost,correct_before_update*1.0/totalEgs,mcc.most_common(1)[0][1]*1.0/totalEgs)
+            print("Test   Cost:%f\tAccuracy:%f\tMCC: %f"%(totalCost,correct_before_update*1.0/totalEgs,mcc.most_common(1)[0][1]*1.0/totalEgs))
 
 
     return totalCost
 
-prevCost=999999
-iter=0
+if __name__ == '__main__':
 
-while True:
+    vocabSize=15000
+    MBSlots=1
+    hiddenDim=12#50#6,12
+    # embeddingSize = 300
+    embeddingSize = 50
 
-    firstCost=trainOrTest(trainingSetIndicesFormat,updateWeights=True, printType="Train")
-    totalCost=trainOrTest(trainingSetIndicesFormat,updateWeights=False, printType="Train")
-    useless=trainOrTest(testSetIndicesFormat,updateWeights=False, printType="Test")
-    if totalCost>prevCost:
-        break
-    if prevCost<totalCost:
-        if iter>20:
-            break
-        print "Costs: Prev: %f\tNew: %f"%(prevCost,totalCost)
-    iter+=1
+    totalIts=40
+    MBSize=(MBSlots,hiddenDim)
+    numLabels=4
+    floatX=theano.config.floatX
+    random_seed = 42
+    rng = np.random.RandomState(random_seed)
+    prevCost=999999
+    iter=0
+
+    modelType = 'word2vecn'
+    filename="../data/pdtb-for-naacl2016/trn-levelone.txt"
+    # filename="../data/correct_data/trn-levelone.txt.10K"
+    test_filename="../data/correct_data/tst-levelone.txt.10K"
+    gloveFile = './glove.6B/glove.6B.'+str(embeddingSize)+'d.txt'
+
+    #load the necessary model
+    print("Preparing data...")
+    if modelType == 'word2vec':
+        print("Loading Word2Vec Embeddings...")
+        model = word2vec.Word2Vec.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
+        print("Done")
+    else:
+        model = data.loadGloveModel(gloveFile)
+
+    #create training and test set
+    vocab = data.buildVocab(filename,vocabSize)
+    trainingSetWordFormat,trainingSetIndicesFormat,trainingSetEmbeddingsFormat=data.buildDataSet(filename,vocab,model) #todo check if its working
+    testSetWordFormat,testSetIndicesFormat,testSetEmbeddingsFormat=data.buildDataSet(test_filename,vocab,model)
+    with open('testTrain-'+str(vocabSize), 'w') as f:  # Python 3: open(..., 'wb')
+        pickle.dump([vocabSize, trainingSetWordFormat,trainingSetIndicesFormat,testSetWordFormat,testSetIndicesFormat, trainingSetEmbeddingsFormat, testSetEmbeddingsFormat], f)
+
+    # # load directly
+    # with open('testTrain-'+str(vocabSize)) as f:  # Python 3: open(..., 'rb')
+    #     vocabSize, trainingSetWordFormat,trainingSetIndicesFormat,testSetWordFormat,testSetIndicesFormat, trainingSetEmbeddingsFormat, testSetEmbeddingsFormat = pickle.load(f)
+
+    print("Done")
+
+    # initMB = np.asarray(rng.normal(loc=0.0, scale=0.1, size=MBSize), dtype=floatX)
+    initMB = np.asarray(np.random.uniform(low = - math.sqrt(6./sum(MBSize)), high=math.sqrt(6./sum(MBSize)), size=MBSize), dtype=floatX)
+    emn=externalMemoryNetworkLSTM.ExternalMemoryNetwork(vocabSize, embeddingSize, hiddenDim, numLabels)
+
+    #todo remove change back
+    # point = int(0.5 * len(trainingSetIndicesFormat))
+    # trainingSetIndicesFormat = trainingSetIndicesFormat[0:point]
+    # trainingSetEmbeddingsFormat = trainingSetEmbeddingsFormat[0:point]
+
+    point = int(0.8 * len(trainingSetIndicesFormat))
+    testSetIndicesFormat = trainingSetIndicesFormat[point:]
+    testSetEmbeddingsFormat = trainingSetEmbeddingsFormat[point:]
+    trainingSetIndicesFormat = trainingSetIndicesFormat[0:point]
+    trainingSetEmbeddingsFormat = trainingSetEmbeddingsFormat[0:point]
+    print("Start Training")
+    learningRate = .05 /.9
+
+    totalCost=trainOrTest(trainingSetIndicesFormat,trainingSetEmbeddingsFormat,updateWeights=False, printType="Train")
+    useless=trainOrTest(testSetIndicesFormat,testSetEmbeddingsFormat,updateWeights=False, printType="Test")
+
+    while True:
+        iter+=1
+        learningRate = learningRate*.9
+        firstCost=trainOrTest(trainingSetIndicesFormat,trainingSetEmbeddingsFormat,updateWeights=True, printType="Train",learningRate=learningRate)
+        totalCost=trainOrTest(trainingSetIndicesFormat,trainingSetEmbeddingsFormat,updateWeights=False, printType="Train")
+        useless=trainOrTest(testSetIndicesFormat,testSetEmbeddingsFormat,updateWeights=False, printType="Test")
+        if prevCost<totalCost:
+            if iter>20:
+                break
+            print("Costs: Prev: %f\tNew: %f"%(prevCost,totalCost))
+
